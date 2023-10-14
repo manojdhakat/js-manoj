09c07291b287fac0e125ec5ef00ca3190b46de44
manojdhakat <147861084+manojdhakat@users.noreply.github.com>
2023-10-14 15:12:22 +0000

    container config done

---
+++ basik/class2.js
@@ -0,0 +1,30 @@
+"use strict"  // treat all js code as newer version
+// alert ( 3 + 3 )  we are using nodesjs. not broser
+
+console.log( 3
+    +
+    3) // code readability should be high
+
+    console.log("manoj")
+
+    let name = "manoj"
+    let age = 20
+    let isLoggedIn = false
+    let state;
+
+    // Number => 2 to power 53
+    // BigInt
+    // String =>""
+    // Boolean =>true/false
+    // null => standalone value 
+    // undefined
+    // symbol =>unique
+
+    // Object
+
+    console.log(typeof age); 
+    
+    console.log(typeof undefined); // undefined
+    console.log(typeof null) // Object 
+
+    
