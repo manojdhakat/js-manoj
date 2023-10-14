afe2f5475a45ca620c7fadd5d5e5040cd586217d
manojdhakat <147861084+manojdhakat@users.noreply.github.com>
2023-10-14 15:55:57 +0000

    container config done

---
+++ basik/class3.js
@@ -0,0 +1,37 @@
+// ConvolverNode Number
+
+
+// let scope = 33
+// let scope = "33"
+// let scope = "33aber"
+// let scope = null
+let scope = undefined
+
+// console.log(typeof scope);    // sope ko done treke se chek kar sakte he
+// console.log(typeof( scope));
+
+let valueInNumber = Number(scope)
+
+// close.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" =>33
+// "33aber" =>NaN
+// true => 1; false => 0
+
+let IsLoggedIn = "manoj"
+
+let BooleanIsLoogedIn = Boolean(IsLoggedIn)
+// console.log(BooleanIsLoogedIn);
+
+// 1 =>true; 0 => false
+//  "" => false
+// "manoj" => true
+
+let someNumber = 33
+
+let StringNumber =String (someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
