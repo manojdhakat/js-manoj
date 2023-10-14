27beae389fe722acb2c5401f31eaa76b4d4a91fc
manojdhakat <147861084+manojdhakat@users.noreply.github.com>
2023-10-14 16:48:25 +0000

    container config done

---
+++ basik/class4.js
@@ -0,0 +1,47 @@
+// let someNumber = 33
+
+// let  StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// ********************operations***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " manoj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2  + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log( (3 + 4) * 5 % 2);  papper ke liy ate he ye lande vaale
+
+// console.logt(+true);
+// console.logt("");
+
+
+// preefix and postfix js mdn site he  pratecla kal ke liye
+let num1, num2, num3
+ num1 =num2 = num3 = 2 + 2
+
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCo);
