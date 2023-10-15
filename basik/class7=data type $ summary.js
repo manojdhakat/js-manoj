be7843b7534a31bf310acad5a0dedfd22dcee779
manojdhakar <147861084+manojdhakat@users.noreply.github.com>
2023-10-15 02:48:01 +0000

    container config done

---
+++ basik/class7=data type $ summary.js
@@ -0,0 +1,22 @@
+// primitive
+ 
+// 7 type: String, Number, Boolean, null, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedId = false
+const outSideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+// Reference (non primitive)
+// Array, Object,function
+
