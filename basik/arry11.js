1d4d356b82d0446bf13ca10a0466b52b3a74ea52
manojdhakar <147861084+manojdhakat@users.noreply.github.com>
2023-10-22 07:34:42 +0000

    container config done

---
+++ basik/arry11.js
@@ -0,0 +1,32 @@
+Array
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors =["manoj","dhakar"]
+
+const Arr2 = new Array(1, 2, 3, 4)
+console.log([myArr[1]])
+
+// Array methods
+
+
+// push sbbhee ko komaa kisaath dikhayegaa pop last valee ho hata degaa
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// unshift array ke first me velue dikhayega  shift aage ke value ko hatatahe
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(-9));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice,    splice  difference
+
